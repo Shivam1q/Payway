d1d90373a36b17753ed664a4d2d0fca72e2782ef
Shivam1q <shivam_y@bt.iitr.ac.in>
2024-09-23 03:02:21 +0530

    middleware.js created

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
 
-import mainRouter from "./routes/index.js"
-
 const app = express();
-
 app.use(cors());
 app.use(express.json())
+
+import mainRouter from "./routes/index.js"
 
 app.use("/api/v1", mainRouter);
 
